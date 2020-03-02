1d9ca5a08e3b60a2fdcad8494fa11026a3ca81ac
Titian3 <47093363+Titian3@users.noreply.github.com>
2020-03-02 22:12:29 +1300

    Add project files.

---
+++ SeeClearlyPOS Console/TerminalConsole.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using SeeClearlyPOS_Library;
+
+namespace SeeClearlyPOS_Console
+{
+    class TerminalConsole
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To seeClearlyPOS");
+            TerminalPOS newTerminalSession = new TerminalPOS();
+            TerminalPOS.Cart shoppingCart = new TerminalPOS.Cart();
+            TerminalPOS.Catalog priceCatalog = new TerminalPOS.Catalog();
+
+            //Set up Products as per spec.
+            priceCatalog.AddDefaultProducts(priceCatalog.newProductCatalog);
+
+            //Check Product list looks good on console.
+            Console.WriteLine("Current Products:");
+            priceCatalog.ShowCurrentProducts(priceCatalog.newProductCatalog);
+
+            //Make sure to start total at 0 prepare interface.
+            bool stillShopping = true;
+            double billRunningTotal = 0;
+
+            //Readline to query prices.
+            /*
+            Arguments:
+                -TC1 -- Runs test case 1.
+                -TC2 -- Runs test case 2.
+                -TC3 -- Runs test case 3.
+                -CART -- Displays current cart items.
+                -CT -- Completes transaction, totals values.
+             */
+            do
+            {
+                Console.WriteLine("Please enter product code:");
+                string terminalInput = Console.ReadLine().ToUpper();
+
+                //Menu - type test or input will be checked for product as normal operation.
+                if (terminalInput == "TC1")
+                {
+                    string[] testCase1 = { "A", "B", "C", "D", "A", "B", "A" };
+                    newTerminalSession.TestCase(testCase1, 13.25, shoppingCart.CurrentCart, priceCatalog.newProductCatalog, billRunningTotal);
+                }
+                else if (terminalInput == "TC2")
+                {
+                    string[] testCase2 = { "C", "C", "C", "C", "C", "C", "C" };
+                    newTerminalSession.TestCase(testCase2, 6.00, shoppingCart.CurrentCart, priceCatalog.newProductCatalog, billRunningTotal);
+                }
+                else if (terminalInput == "TC3")
+                {
+                    string[] testCase3 = { "A", "B", "C", "D" };
+                    newTerminalSession.TestCase(testCase3, 7.25, shoppingCart.CurrentCart, priceCatalog.newProductCatalog, billRunningTotal);
+                }
+                else if (terminalInput == "CT")
+                {
+                    newTerminalSession.Calculatetotal(shoppingCart.CurrentCart, priceCatalog.newProductCatalog, billRunningTotal);
+                    stillShopping = false;
+                }
+                else if (terminalInput == "CART")
+                {
+                    //Show Current Cart contents
+                    shoppingCart.ShowCartContents(shoppingCart.CurrentCart);
+                }
+                else
+                {
+                    //Check if it is a product in the product list
+                    newTerminalSession.IsRealProduct(shoppingCart.CurrentCart, priceCatalog.newProductCatalog, terminalInput);
+                }
+            }
+            while (stillShopping);
+        }
+    }
+}
---
+++ SeeClearlyPOS Library/Terminal.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeeClearlyPOS_Library
+{
+    public class Terminal
+    {
+        public class ProductList
+        {
+            public string ProductCode { get; set; }
+            public double Price { get; set; }
+            public bool HasBulk { get; set; }
+            public int BulkTrigger { get; set; }
+            public double BulkDiscount { get; set; }
+
+            public override string ToString()
+            {
+                return "ProductCode: " + ProductCode + "   Price: " + Price;
+            }
+        }
+
+        public class Catalog
+        {
+            public List<ProductList> newProductCatalog = new List<ProductList>();
+
+            public void AddDefaultProducts(List<ProductList> newCatalog)
+            {
+                newCatalog.Add(new ProductList { ProductCode = "A", Price = 1.25, HasBulk = true, BulkTrigger = 3, BulkDiscount = 0.75 });
+                newCatalog.Add(new ProductList { ProductCode = "B", Price = 4.25, HasBulk = false });
+                newCatalog.Add(new ProductList { ProductCode = "C", Price = 1.00, HasBulk = true, BulkTrigger = 6, BulkDiscount = 1 });
+                newCatalog.Add(new ProductList { ProductCode = "D", Price = 0.75, HasBulk = false });
+            }
+            public void ShowCurrentProducts(List<ProductList> PriceCatalog)
+            {
+                foreach (ProductList product in PriceCatalog)
+                {
+                    Console.WriteLine(product);
+                }
+            }
+        }
+
+        public class Cart
+        {
+            public List<string> CurrentCart = new List<string>();
+
+            public void ShowCartContents(List<string> cartContents)
+            {
+                Console.WriteLine("Current Cart:");
+                foreach (string item in cartContents)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            public void ClearCurrentCart()
+            {
+                CurrentCart.Clear();
+            }
+
+        }
+
+        public bool IsRealProduct(List<string> shoppingCart, List<ProductList> priceCatalog, string terminalInput)
+        {
+            bool inCatalog = priceCatalog.Exists(x => x.ProductCode == terminalInput);
+            if (inCatalog == true)
+            {
+                shoppingCart.Add(terminalInput);
+                Console.WriteLine("Current Cart:");
+                foreach (string item in shoppingCart)
+                {
+                    Console.WriteLine(item);
+                }
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry it looks like your Product is not in the Catalog");
+                return false;
+            }
+        }
+        //Check if it is a product
+
+        public double Calculatetotal(List<string> shoppingCart, List<ProductList> priceCatalog, double billRunningTotal)
+        {
+            //Check for bulk discounts
+            var cartCount = shoppingCart.GroupBy(x => x)
+            .Select(product => new { ProductCodeName = product.Key, ProductCodeCount = product.Count() })
+            .ToList();
+            foreach (var key in cartCount)
+            {
+                Console.WriteLine("Currently in the cart: {0} x {1}", key.ProductCodeCount, key.ProductCodeName);
+                var productRef = priceCatalog.Find(x => x.ProductCode.Contains(key.ProductCodeName));
+                if (productRef.HasBulk == true)
+                {
+                    billRunningTotal = billRunningTotal + (productRef.Price * key.ProductCodeCount);
+                    double discountsApplied = key.ProductCodeCount / productRef.BulkTrigger;
+                    Math.Floor(discountsApplied);
+                    if (discountsApplied > 0)
+                    {
+                        double discountAmount = productRef.BulkDiscount * discountsApplied;
+                        Console.WriteLine("Special Discount: -${0}", discountAmount);
+                        billRunningTotal = billRunningTotal - discountAmount;
+                    }
+                }
+                else
+                {
+                    billRunningTotal = billRunningTotal + (productRef.Price * key.ProductCodeCount);
+                }
+            }
+            Console.WriteLine("Total To Pay: ${0}", billRunningTotal);
+            return billRunningTotal;
+        }
+        public void TestCase(string[] caseArray, double targetValue, List<string> shoppingCart, List<ProductList> priceCatalog, double billRunningTotal)
+        {
+            shoppingCart.InsertRange(0, caseArray);
+            double results = Calculatetotal(shoppingCart, priceCatalog, billRunningTotal);
+            if (results == targetValue)
+            {
+                Console.WriteLine("Test Case Passed");
+                shoppingCart.Clear();
+            }
+        }
+    }
+}
+
